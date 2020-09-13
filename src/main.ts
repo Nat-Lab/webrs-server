faa0eab586ea6f591928f5b870b585d1c8c57898
Maho Morichika <nat@magicnat.com>
2020-09-12 22:31:02 -0400

    use pty

--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,28 @@
-import { connect } from 'http2';
-import { Duplex } from 'stream';
-const Telnet = require('telnet-client');
-const WebSocket = require('ws');
+import * as NodePty from 'node-pty';
+import * as ws from 'ws';
 
-let run = async function (server: string, port: number, encoding: BufferEncoding = 'utf-8') {
-    let wss = new WebSocket.Server({
+let run = async function (rsBin: string) {
+    let wss = new ws.Server({
         port: 8080,
         host: "0.0.0.0"
     });
 
     wss.on('connection', async function connection(ws: any) {
-        console.log('client connected, connecting to rs...');
-
-        let telnet = new Telnet();
-        let ldata: string = '';
-        let skip = 0;
-
-        await telnet.connect({
-            host: server,
-            port: port,
-            negotiationMandatory: false
+        let rsShell = NodePty.spawn(rsBin, [], {
+            name: 'xterm-color',
+            cols: 80,
+            rows: 24,
         });
 
-        let stream: Duplex = await telnet.shell();
+        rsShell.onData(data => ws.send(data));
+        rsShell.onExit(() => ws.close());
 
-        console.log('connected to rs.');
-
-        ws.on('message', (message: any) => {
-            ldata = message;
-            stream.write(message);
+        ws.on('message', (data: string) => {
+            rsShell.write(data);
         });
 
-        ws.on('close', () => {
-            console.log('client left, disconnecting rs.');
-            telnet.end();
-        });
-
-        telnet.on('end', () => {
-            console.log('rs left, kicking client.');
-            ws.close();
-        });
-
-        stream.on('data', (data: Buffer) => {
-            if (skip < 3) {
-                skip++;
-                return;
-            }
-            let datastr: string = data.toString(encoding);
-            if (datastr != ldata) {
-                ws.send(datastr);
-            }
-            ldata = '';
-        });
+        ws.on('end', () => rsShell.kill());
     });
 }
 
-run("rs.nat.moe", 23);
+run('/usr/bin/routeserver');
